5bf551baa5fc7f38190bb8251cba5715637c9a74
Frederike Duembgen <frederike.dumbgen@inria.fr>
2025-05-26 10:41:54 -0400

    Enable all tests

--- popr/utils/test_tools.py
+++ popr/utils/test_tools.py
@@ -18,11 +18,11 @@ n_landmarks = 3
 n_poses = 4
 # fmt: off
 Lifters = [
-    #(Poly4Lifter, dict()),
-    #(Poly6Lifter, dict()),
-    #(WahbaLifter, dict(n_landmarks=3, d=2, robust=False, level="no", n_outliers=0)),
-    #(MonoLifter, dict(n_landmarks=5, d=2, robust=False, level="no", n_outliers=0)),
-    #(WahbaLifter, dict(n_landmarks=5, d=2, robust=True, level="xwT", n_outliers=1)),
+    (Poly4Lifter, dict()),
+    (Poly6Lifter, dict()),
+    (WahbaLifter, dict(n_landmarks=3, d=2, robust=False, level="no", n_outliers=0)),
+    (MonoLifter, dict(n_landmarks=5, d=2, robust=False, level="no", n_outliers=0)),
+    (WahbaLifter, dict(n_landmarks=5, d=2, robust=True, level="xwT", n_outliers=1)),
     (MonoLifter, dict(n_landmarks=6, d=2, robust=True, level="xwT", n_outliers=1)),
     (RangeOnlyLocLifter,  dict(n_positions=n_poses, n_landmarks=n_landmarks, d=d, level="no")),
     (RangeOnlyLocLifter, dict(n_positions=n_poses, n_landmarks=n_landmarks, d=d, level="quad")),
